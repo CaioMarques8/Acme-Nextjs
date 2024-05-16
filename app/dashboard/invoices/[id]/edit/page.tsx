9a18167627adb2bcf1a1a2dec124356317e8f503
Caio <Caio.Marques883@gmail.com>
2024-05-16 20:10:51 -0300

    adicionando metadados

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,6 +4,12 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
  
 import { notFound } from 'next/navigation';
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata ={
+  title:'Edit',
+}
+
 export default async function Page({ params }: { params: { id: string } }){
 
   const id = params.id;
